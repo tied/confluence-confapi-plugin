e4a065a160d34f00259e8d4e7e0cadaabf5c98eb
Kai Lehmann <kai.uni-assist@lekatech.de>
2020-03-26 14:06:12 +0100

    license resource implemented
    The license resource can be used to read or set the current Confluence
    product license.

---
+++ src/test/java/de/aservo/atlassian/confluence/confapi/rest/LicenseResourceTest.java
@@ -0,0 +1,77 @@
+package de.aservo.atlassian.confluence.confapi.rest;
+
+import com.atlassian.sal.api.i18n.InvalidOperationException;
+import com.atlassian.sal.api.license.LicenseHandler;
+import com.atlassian.sal.api.license.SingleProductLicenseDetailsView;
+import de.aservo.atlassian.confapi.model.ErrorCollection;
+import de.aservo.atlassian.confapi.model.LicenseBean;
+import de.aservo.atlassian.confapi.model.LicensesBean;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.ws.rs.core.Response;
+
+import static com.atlassian.confluence.setup.ConfluenceBootstrapConstants.DEFAULT_LICENSE_REGISTRY_KEY;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LicenseResourceTest {
+
+    private LicenseHandler licenseHandler;
+    private LicenceResource resource;
+
+    @Before
+    public void setup() {
+        licenseHandler = mock(LicenseHandler.class);
+        resource = new LicenceResource(licenseHandler);
+    }
+
+    @Test
+    public void testGetLicense() {
+        SingleProductLicenseDetailsView view = mock(SingleProductLicenseDetailsView.class);
+
+        doReturn(view).when(licenseHandler).getProductLicenseDetails(DEFAULT_LICENSE_REGISTRY_KEY);
+
+        final Response response = resource.getLicenses();
+        assertEquals(200, response.getStatus());
+        final LicensesBean licensesBean = (LicensesBean) response.getEntity();
+
+        assertEquals(licensesBean.getLicenses().iterator().next(), new LicenseBean(view));
+    }
+
+    @Test
+    public void testGetLicenseWithError() {
+        doThrow(new RuntimeException()).when(licenseHandler).getProductLicenseDetails(DEFAULT_LICENSE_REGISTRY_KEY);
+
+        final Response response = resource.getLicenses();
+        assertEquals(400, response.getStatus());
+
+        assertNotNull(response.getEntity());
+        assertEquals(ErrorCollection.class, response.getEntity().getClass());
+    }
+
+    @Test
+    public void testSetLicense() {
+        SingleProductLicenseDetailsView view = mock(SingleProductLicenseDetailsView.class);
+
+        doReturn(view).when(licenseHandler).getProductLicenseDetails(DEFAULT_LICENSE_REGISTRY_KEY);
+
+        final Response response = resource.setLicense(null, "ABCDEFG");
+        assertEquals(200, response.getStatus());
+    }
+
+    @Test
+    public void testSetLicenseWithError() throws InvalidOperationException {
+        doThrow(new RuntimeException()).when(licenseHandler).addProductLicense(any(String.class), any(String.class));
+
+        final Response response = resource.setLicense(null,"ABCDEFG");
+        assertEquals(400, response.getStatus());
+
+        assertNotNull(response.getEntity());
+        assertEquals(ErrorCollection.class, response.getEntity().getClass());
+    }
+}
