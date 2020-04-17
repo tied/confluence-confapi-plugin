ce70a7f8f3c76a885f7bca138c4233597bbbce78
Patrick Hobusch <patrick.hobusch@gmail.com>
2020-04-17 13:24:09 +0800

    Adjust initial user resource implementation to latest standards
    Generic files like the user bean, the user service implementation and the user
    resource implementation have been moved to the ConfAPI commons library while the
    actual service implementation is kept at the plugin itself.
    
    In order to have a 'centralized' translation of user and user beans, a new user
    bean util has been introduced. The service and the resource as possible
    alternatives to this util turned out to be hard to handle.

--- src/test/java/de/aservo/atlassian/confluence/confapi/service/UserServiceTest.java
+++ src/test/java/de/aservo/atlassian/confluence/confapi/service/UserServiceTest.java
@@ -1,21 +1,23 @@
 package de.aservo.atlassian.confluence.confapi.service;
 
-import com.atlassian.confluence.user.ConfluenceUserImpl;
-import com.atlassian.crowd.exception.UserNotFoundException;
 import com.atlassian.user.EntityException;
 import com.atlassian.user.User;
 import com.atlassian.user.UserManager;
 import com.atlassian.user.impl.DefaultUser;
-import de.aservo.atlassian.confluence.confapi.model.UserBean;
+import de.aservo.atlassian.confapi.exception.BadRequestException;
+import de.aservo.atlassian.confapi.exception.NotFoundException;
+import de.aservo.atlassian.confapi.model.UserBean;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static de.aservo.atlassian.confluence.confapi.model.util.UserBeanUtil.toUser;
+import static de.aservo.atlassian.confluence.confapi.model.util.UserBeanUtil.toUserBean;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doReturn;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
@@ -23,90 +25,82 @@ public class UserServiceTest {
     @Mock
     private UserManager userManager;
 
-    private UserService userService;
+    private UserServiceImpl userService;
 
     @Before
     public void setup() {
-        userService = new UserService(userManager);
+        userService = new UserServiceImpl(userManager);
     }
 
     @Test
-    public void testGetUser() throws EntityException, UserNotFoundException {
-        User user = createUser();
+    public void testGetUser() throws EntityException, NotFoundException {
+        User user = toUser(UserBean.EXAMPLE_1);
         doReturn(user).when(userManager).getUser(user.getName());
 
-        UserBean userBean = userService.getUser(user.getName());
+        UserBean userBean = toUserBean(user);
+        UserBean gotUserBean = userService.getUser(user.getName());
 
-        assertEquals(userBean, new UserBean(user));
+        assertEquals(userBean, gotUserBean);
         assertNull(userBean.getPassword());
     }
 
-    @Test(expected = UserNotFoundException.class)
-    public void testGetUserIsNotConfluenceUser() throws EntityException, UserNotFoundException {
-        User user = new DefaultUser("test", "test user", "user@user.de");
-
+    @Test(expected = NotFoundException.class)
+    public void testGetUserIsNotConfluenceUser() throws NotFoundException, EntityException {
+        User user = new DefaultUser(toUser(UserBean.EXAMPLE_1));
         doReturn(user).when(userManager).getUser(user.getName());
-
         userService.getUser(user.getName());
     }
 
     @Test
-    public void testUpdateUser() throws EntityException, UserNotFoundException, IllegalAccessException {
-        User user = createUser();
-        UserBean userBeanUpdate = new UserBean(user);
-
+    public void testUpdateUser() throws EntityException, NotFoundException, BadRequestException {
+        final User user = toUser(UserBean.EXAMPLE_1);
         doReturn(user).when(userManager).getUser(user.getName());
 
-        UserBean userBean = userService.updateEmail(userBeanUpdate);
+        final UserBean updateUserBean = UserBean.EXAMPLE_2;
+        final UserBean updatedUserBean = userService.updateUser(user.getName(), updateUserBean);
 
-        assertEquals(userBean, userBeanUpdate);
-    }
-
-    @Test(expected = UserNotFoundException.class)
-    public void testUpdateUserNotConfluenceUser() throws EntityException, UserNotFoundException, IllegalAccessException {
-        User user = new DefaultUser("test", "test user", "user@user.de");
-        UserBean userBeanUpdate = new UserBean(user);
-
-        doReturn(user).when(userManager).getUser(user.getName());
-
-        userService.updateEmail(userBeanUpdate);
+        assertEquals(updateUserBean, updatedUserBean);
     }
 
     @Test
-    public void testUpdateExceptionOnWriteEmailField() throws EntityException, UserNotFoundException, IllegalAccessException {
-        User user = createUser();
-        UserBean userBean = new UserBean(user);
-        ConfluenceUserImpl confluenceUser = (ConfluenceUserImpl) user;
-
+    public void testUpdateUserEmptyBean() throws EntityException, NotFoundException, BadRequestException {
+        final User user = toUser(UserBean.EXAMPLE_1);
         doReturn(user).when(userManager).getUser(user.getName());
-        doThrow(new RuntimeException()).when(userManager).saveUser(confluenceUser);
 
-        UserBean userBeanUpdated = userService.updateEmail(userBean);
+        final UserBean updateUserBean = new UserBean();
+        final UserBean notUpdatedUserBean = userService.updateUser(user.getName(), updateUserBean);
 
-        assertEquals(userBeanUpdated, userBean);
+        assertEquals(UserBean.EXAMPLE_1, notUpdatedUserBean);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void testUpdateUserNotConfluenceUser() throws EntityException, NotFoundException, BadRequestException {
+        final User user = new DefaultUser(toUser(UserBean.EXAMPLE_1));
+        doReturn(user).when(userManager).getUser(user.getName());
+
+        userService.updateUser(user.getName(), UserBean.EXAMPLE_2);
     }
 
     @Test
-    public void testUpdateUserPassword() throws EntityException, UserNotFoundException {
-        User user = createUser();
-        UserBean userBeanUpdate = new UserBean(user);
-        doReturn(user).when(userManager).getUser(user.getName());
+    public void testUpdateUserPassword() throws EntityException, NotFoundException, BadRequestException {
+        final UserBean userBean = UserBean.EXAMPLE_1;
+        doReturn(toUser(userBean)).when(userManager).getUser(userBean.getUserName());
 
-        UserBean userBean = userService.updatePassword(user.getName(), "newPW");
+        final UserBean updateUserBean = new UserBean();
+        updateUserBean.setPassword("new password");
 
-        assertEquals(userBean, userBeanUpdate);
+        final UserBean updatedUserBean = userService.updateUser(userBean.getUserName(), userBean);
+        assertEquals(userBean, updatedUserBean);
+        // user password is not returned here, getting user bean shows update was successful
+        assertNull(updatedUserBean.getPassword());
     }
 
-    @Test(expected = UserNotFoundException.class)
-    public void testUpdateUserPasswordNotConfluenceUser() throws EntityException, UserNotFoundException {
-        User user = new DefaultUser("test", "test user", "user@user.de");
-
+    @Test(expected = NotFoundException.class)
+    public void testUpdateUserPasswordNotConfluenceUser() throws EntityException, NotFoundException, BadRequestException {
+        final User user = new DefaultUser(toUser(UserBean.EXAMPLE_1));
         doReturn(user).when(userManager).getUser(user.getName());
 
         userService.updatePassword(user.getName(), "newPW");
     }
 
-    private User createUser() {
-        return new ConfluenceUserImpl("test", "test user", "user@user.de");
-    }
 }
