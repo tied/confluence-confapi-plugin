2f84c123390e653dfb76f7c147cc368070bcca57
Patrick Hobusch <patrick.hobusch@gmail.com>
2020-04-17 18:45:10 +0800

    Use swagger annotated resource interface (PoC)
    It is possible to move the swagger annotation to resource interfaces. This has
    the benefit that all features (annotations) of swagger (that make the code very
    hard to read) can be used apart from the actual implementation but still in the
    code. The other large benefit is that the same documentation can be shared
    amongst all plugins (if needed).

--- src/main/java/de/aservo/atlassian/confluence/confapi/rest/PingResource.java
+++ src/main/java/de/aservo/atlassian/confluence/confapi/rest/PingResource.java
@@ -2,8 +2,7 @@ package de.aservo.atlassian.confluence.confapi.rest;
 
 import com.atlassian.plugins.rest.common.security.AnonymousAllowed;
 import de.aservo.atlassian.confapi.constants.ConfAPI;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import de.aservo.atlassian.confapi.rest.PingResourceInterface;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -13,19 +12,11 @@ import javax.ws.rs.core.Response;
 
 @Path(ConfAPI.PING)
 @AnonymousAllowed
-@Produces({MediaType.APPLICATION_JSON})
-public class PingResource {
-
-    public static final String PONG = "pong";
+@Produces(MediaType.TEXT_PLAIN)
+public class PingResource implements PingResourceInterface {
 
     @GET
-    @Operation(
-            tags = { ConfAPI.PING },
-            summary = "Simple ping method for probing the REST api. Returns 'pong' upon success",
-            responses = {
-                    @ApiResponse(responseCode = "200"),
-            }
-    )
+    @Override
     public Response getPing() {
         return Response.ok(PONG).build();
     }
